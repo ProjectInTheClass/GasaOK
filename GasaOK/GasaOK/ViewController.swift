45c62bb33948643029afb869de382cfa21087ac5
dahae0320 <ldh990320ldh@gmail.com>
2022-02-12 17:01:01 +0900

    frist commit

---
+++ GasaOK/GasaOK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GasaOK
+//
+//  Created by Da Hae Lee on 2022/02/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
