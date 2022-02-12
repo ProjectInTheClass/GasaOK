d932e12c455bbb981968aa4c73d18b4d36f7ea2d
dahae0320 <ldh990320ldh@gmail.com>
2022-02-12 16:13:09 +0900

    Initial Commit

---
+++ GasaOK/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  GasaOK
+//
+//  Created by Da Hae Lee on 2022/02/12.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
