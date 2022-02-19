5ea5365ba70acd31d09bb0db6f2d6e0634066ce4
dahae0320 <ldh990320ldh@gmail.com>
2022-02-19 23:22:37 +0900

    [22-02-19] search 화면 구현 완 (#2)

--- 가사오케/GasaOK/Model/searchResultDummy.swift
+++ 가사오케/GasaOK/Model/searchResultDummy.swift
@@ -17,4 +17,8 @@ let searchResultDummy = [
     SongInfo(songName: "우리가 헤어져야 했던 이유", singerName: "비비", karaokeNumber: "56713", lryics: nil, karaokeType: .KY),
     SongInfo(songName: "나무", singerName: "카더가든", karaokeNumber: "69031", lryics: nil, karaokeType: .KY),
     SongInfo(songName: "이별후회", singerName: "김나영", karaokeNumber: "78942", lryics: nil, karaokeType: .KY),
+    SongInfo(songName: "위잉위잉", singerName: "혁오", karaokeNumber: "ky", lryics: nil, karaokeType: .KY),
+    SongInfo(songName: "혁오", singerName: "위잉위잉", karaokeNumber: "tj", lryics: nil, karaokeType: .TJ),
+    SongInfo(songName: "혁오", singerName: "위잉위잉----tj", karaokeNumber: "tj", lryics: nil, karaokeType: .TJ),
+    SongInfo(songName: "혁오", singerName: "위잉위잉----ky", karaokeNumber: "ky", lryics: nil, karaokeType: .KY),
 ]
