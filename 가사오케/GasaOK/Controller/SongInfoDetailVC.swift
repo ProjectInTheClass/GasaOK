a4757338856e92d6e6a5a0f2685b59f17d895156
Leebomyi <tak1203@naver.com>
2022-02-23 19:40:17 +0900

    [2022-02-23] 네비게이션바 백버튼, 탭바 숨김 이슈 해결

--- 가사오케/GasaOK/Controller/SongInfoDetailVC.swift
+++ 가사오케/GasaOK/Controller/SongInfoDetailVC.swift
@@ -14,30 +14,17 @@ class SongInfoDetailVC: UIViewController {
     @IBOutlet weak var songNameLabel: UILabel!
     @IBOutlet weak var singerNameLabel: UILabel!
     @IBOutlet weak var karaokaNumberLabel: UILabel!
+    @IBOutlet weak var lyricsLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // nav bar 가사 화면 네이비게이션 뒤로가기버튼 설정
-        self.navigationController?.navigationBar.topItem?.title = ""
-        // 가사 상세 보기화면에서 탭 숨기기
-        self.tabBarController?.tabBar.isHidden = true
-        // Do any additional setup after loading the view.
+        songInfodidshow()
+    }
+    
+    func songInfodidshow() {
         songNameLabel.text = songDetailData.songName
         singerNameLabel.text = songDetailData.singerName
         karaokaNumberLabel.text = songDetailData.karaokeNumber
-        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
